3db071c5b7aab62d487b023dc3944e007788fc7d
kimvykz <kimslavakz@gmail.com>
2023-01-21 20:54:58 +0600

    beta 0001

--- program.py
+++ program.py
@@ -1,1 +1,13 @@
-print('hello world!')
+import datetime as dt
+
+class Calculator:
+    def __init__(self, limit):
+        self.limit = limit
+        records = []
+
+class Record:
+    def __init__(self, amount, comment, date)
+
+transactions = {
+    '2023-01-21' : 123
+                }

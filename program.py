03113c098875d1adca8bcdd6c000c4f20d5b400f
kimvykz <kimslavakz@gmail.com>
2023-01-23 10:01:07 +0600

    beta 0005

--- program.py
+++ program.py
@@ -3,19 +3,23 @@ import datetime as dt
 date_format = '%d.%m.%Y'
 today_ = dt.datetime.now()
 today_str_ = today_.strftime(date_format)
+week_dates_list_ = []
+
+for day_ in range(0,7):
+    week_dates_list_.append((today_ - dt.timedelta(days=day_)).strftime(date_format))
+    
 
 class Record:
     def __init__(self, amount, comment, date):
         self.amount = amount
         self.comment = comment
         self.date = date
-
     
 
 class Calculator:
     def __init__(self, limit):
         self.limit = limit
-        records = []
+        #records = []
 
 
 class CaloriesCalculator(Calculator):
@@ -43,10 +47,19 @@ class CaloriesCalculator(Calculator):
         return res_
 
     def get_today_stats(self):
-        return ''
+        res_ = 0
+        for today_ate_ in self.ate_food_:
+            if today_ate_[2] == today_str_:
+                res_ += today_ate_[0]
+        return f'На сегодня употребленно: {res_} кКал'
 
     def get_week_stats(self):
-        return ''
+        res_ = 0
+
+        for food_ in self.ate_food_:
+            if food_[2] in week_dates_list_:
+                res_ += food_[0]
+        return f'За последнюю неделю съедено: {res_} кКал'
 
 class CashCalculator(Calculator):
     
@@ -80,11 +93,21 @@ class CashCalculator(Calculator):
         return res_
 
     def get_today_stats(self):
-        return ''
+        res_ = 0
 
-    def get_week_stats(self):    
-        return ''
+        for today_spent_ in self.transactions_:
+            if today_spent_[2] == today_str_:
+                res_ += today_spent_[0]
 
+        return f'На сегодня уже потрачено: {res_} {self.currency}'
+
+    def get_week_stats(self):   
+        res_ = 0
+
+        for spent_ in self.transactions_:
+            if spent_[2] in week_dates_list_:
+                res_ += spent_[0] 
+        return f'За последнюю неделю потрачено {res_} {self.currency}'
 
 
 cash_calculator = CashCalculator(1000)
@@ -94,15 +117,25 @@ cash_calculator.add_record(Record(150,'coffe','21.01.2023'))
 cash_calculator.add_record(Record(75,'tea','19.01.2023'))
 cash_calculator.add_record(Record(700,'dinner','21.01.2023'))
 cash_calculator.add_record(Record(50,'lunch','21.01.2023'))
+cash_calculator.add_record(Record(75,'tea','23.01.2023'))
+cash_calculator.add_record(Record(700,'dinner','23.01.2023'))
+cash_calculator.add_record(Record(50,'lunch','23.01.2023'))
 
 cal_calculator.add_record(Record(500,'salad','21.01.2023'))
 cal_calculator.add_record(Record(700,'pelmeni','21.01.2023'))
 cal_calculator.add_record(Record(600,'shashlyq','21.01.2023'))
+cal_calculator.add_record(Record(400,'salad','23.01.2023'))
+cal_calculator.add_record(Record(900,'pelmeni','23.01.2023'))
+cal_calculator.add_record(Record(850,'shashlyq','23.01.2023'))
 
 
 
 print(cash_calculator.get_today_cash_remained('KZT'))
 print(cal_calculator.get_calories_remained())
+print(cal_calculator.get_today_stats())
+print(cash_calculator.get_today_stats())
+print(cal_calculator.get_week_stats())
+print(cash_calculator.get_week_stats())
 
 #print(cash_calculator.transactions_)
          
